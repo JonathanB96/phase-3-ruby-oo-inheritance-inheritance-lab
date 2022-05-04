62bd6bbb6161d24e273c8a4c53024a79f0e6308f
jonathan bola <bolajonathan@yahoo.com>
2022-05-04 17:15:11 -0500

    commit tree 6bbc04575638448902b4ffea3210d5f32fabd756

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+#require File.expand_path('../user.rb', __FILE__)
+
+class Student < User
+    attr_accessor:knowledge
+    
+    def initialize
+        @knowledge = []
+
+    end
+   
+    
+    def learn(str)
+    @knowledge << str
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,9 @@
-class Teacher
+#require File.expand_path('../user.rb', __FILE__)
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
+ def teach
+  KNOWLEDGE.sample
+ end
   
 end
